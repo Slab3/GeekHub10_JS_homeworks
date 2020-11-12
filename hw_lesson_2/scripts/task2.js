66ae26339afac01ee75c07efab49926b268a7603
Slab3 <zshylga@gmail.com>
2020-11-12 19:45:59 +0200

    tasks js fix

---
+++ hw_lesson_2/scripts/task2.js
@@ -0,0 +1,30 @@
+console.log('---');
+console.log('Task 2: ');
+
+let arrHeight1 = [2, 1, 5, 0, 3, 4, 7, 2, 3, 1, 0];
+let arrHeight2 = [3, 4, 4, 5, 3, 4, 0, 3, 6, 1, 4];
+
+function needToFillTheCells(arrHeight, lgth) {
+    let stock = 0;
+
+    for (let i = 0; i < lgth; i++) {
+
+        let left = arrHeight[i];
+        for (let j = 0; j < i; j++) {
+            left = Math.max(left, arrHeight[j]);
+        }
+
+        let right = arrHeight[i];
+        for (let j = i; j < lgth; j++) {
+            right = Math.max(right, arrHeight[j]);
+        }
+        stock += Math.min(left, right) - arrHeight[i];
+    }
+    return stock;
+}
+
+lgth = arrHeight1.length;
+console.log('Array 1: ' + needToFillTheCells(arrHeight1, lgth));
+
+lgth = arrHeight2.length;
+console.log('Array 2: ' + needToFillTheCells(arrHeight2, lgth));
