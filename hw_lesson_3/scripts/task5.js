0bb81167673cf64242ebcd4bb41b73a5fb142988
Slab3 <zshylga@gmail.com>
2020-11-22 17:48:32 +0200

    task 5 - myFilter & myFind

--- hw_lesson_3/scripts/task5.js
+++ hw_lesson_3/scripts/task5.js
@@ -1,2 +1,30 @@
 console.log('Task 5');
 
+// myForEach
+
+// myMap
+
+// mySort
+
+// myFilter
+
+Array.prototype.myFilter = function (callback, thisArg = this) {
+    let newArr = [];
+
+    for (let i = 0; i < this.length; i++) {
+        if (callback.call(null, this[i], i, this)) newArr.push(this[i]);
+    }
+
+    return newArr;
+}
+
+
+// myFind
+
+Array.prototype.myFind = function (callback, thisArg = this) {
+    for (let i = 0; i < this.length; i++) {
+        if (callback.call(null, this[i], i, this) === true) {
+            return this[i];
+        }
+    }
+};
