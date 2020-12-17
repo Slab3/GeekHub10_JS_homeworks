ff57a267776c6b97a94e6205afe19b868a2d8734
Slab3 <zshylga@gmail.com>
2020-12-17 23:22:44 +0200

    slider - dots click events

--- hw_lesson_5/scripts/main-lesson.js
+++ hw_lesson_5/scripts/main-lesson.js
@@ -33,14 +33,5 @@
 //=--------------------------------------------------------------------------------------------------------=
 // debounce
 
-//
-let timeout = null;
 
-let input = document.createElement('input');
-input.addEventListener('keyup', function (e) {
-    clearTimeout(timeout);
-    timeout = setTimeout(() => {console.log(e)}, 300)
-});
-
-$0.append('input');
 
