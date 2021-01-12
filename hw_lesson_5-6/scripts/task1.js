0b288610f338e58a8aaf39f5533981cfb0f86e9a
Slab3 <zshylga@gmail.com>
2021-01-12 21:49:33 +0200

    perform

--- hw_lesson_5-6/scripts/task1.js
+++ hw_lesson_5-6/scripts/task1.js
@@ -1,20 +1,32 @@
-// myPromise
+function perform(...args) {
+    let arrArgs = [...args].slice(0, [...args].length - 1);
+    const func = [...args].find(elem => typeof elem === "function");
+    arrArgs = func(parseInt(arrArgs));
+    return {
+        arrArgs,
+        func,
+        then(...args) {
+            const newArgs = [...args].slice(0, [...args].length - 1);
+            this.func = [...args].find(elem => typeof elem === "function");
+            this.arrArgs = this.func(...newArgs, this.arrArgs);
+            return this;
+        }
+    }
+}
 
-// function perform() {
-//     //TODO implement
-// }
-//
-//     perform(20, function (value) {
-//         console.log(value); //20
-//         var param = 1;
-//         console.log(param); //1
-//         return param;
-//     })
-//     .then('a','b', function (a, b, param) {
-//     console.log(++param); //2
-//     return param;
-//     })
-//     .then(function (param) { //param === 2
-//     console.log(++param); //3
-//         return param;
-//     });
+perform(20, function(value) {
+    console.log(value); // 20
+    var param = 1;
+    console.log(param); // 1
+    return param;
+})
+    .then('a', 'b', function(a, b, param) {
+        console.log(++param); // 2
+        return param;
+    })
+    .then(function(param) { // param === 2
+        console.log(++param); // 3
+        return param;
+    });
+
+
